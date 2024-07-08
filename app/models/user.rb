0ba39c04f7ad85bbcdfec572f1f9ea13611c4e49
Ahkshaey Ravi <ahkshaey@gmail.com>
2024-07-08 22:43:47 +0530

    wired all api endpoints as per the requirement

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+
+    validates_uniqueness_of :email
+    
+    scope :filter_by_campaign_name, ->  (name) {
+        where("JSON_CONTAINS(campaigns_list, ?)", { campaign_name: name }.to_json)
+    }
+
 end
