0945016b5d5fdded883db96d0145bbf25677b669
Ahkshaey Ravi <ahkshaey@gmail.com>
2024-07-09 21:07:12 +0530

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,12 @@
 
 # Seed sample user data into users table
 User.create([{name: "John", email: "john@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam2", "campaign_id": "id2"}]},
-             {name: "Jane", email: "jane@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam3", "campaign_id": "id3"}]}])
+             {name: "Jane", email: "jane@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam3", "campaign_id": "id3"}]},
+             {name: "Julie", email: "julie@gmail.com", campaigns_list: [{"campaign_name": "cam2", "campaign_id": "id2"}, {"campaign_name": "cam3", "campaign_id": "id3"}]},
+             {name: "Judeth", email: "judeth@gmail.com", campaigns_list: [{"campaign_name": "cam3", "campaign_id": "id3"}]},
+             {name: "Jack", email: "jack@gmail.com", campaigns_list: [{"campaign_name": "cam4", "campaign_id": "id4"}, {"campaign_name": "cam2", "campaign_id": "id2"}]},
+             {name: "Jones", email: "jones@gmail.com", campaigns_list: [{"campaign_name": "cam4", "campaign_id": "id4"}]},
+             {name: "Janet", email: "janet@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam3", "campaign_id": "id3"}, {"campaign_name": "cam5", "campaign_id": "id5"}]},
+             {name: "Robert", email: "robert@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam5", "campaign_id": "id5"}]},
+             {name: "Varun", email: "varun@gmail.com", campaigns_list: [{"campaign_name": "cam1", "campaign_id": "id1"}, {"campaign_name": "cam4", "campaign_id": "id4"}]},
+             {name: "Nidhi", email: "nidhi@gmail.com", campaigns_list: [{"campaign_name": "cam5", "campaign_id": "id5"}, {"campaign_name": "cam2", "campaign_id": "id2"}]}])
